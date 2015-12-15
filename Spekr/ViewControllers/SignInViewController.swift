52eece56d1956a77719f9fc018c8223e2a69088a
Arjun <itsarjunn@gmail.com>
2015-12-15 10:41:51 +0530

    Sign in view controller has been added(including labels and buttons)

---
+++ Spekr/ViewControllers/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  Spekr
+//
+//  Created by Arjun Kodur on 12/14/15.
+//  Copyright © 2015 Arjun Kodur. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidAppear(animated: Bool) {
+        self.view.backgroundColor = UIColor.init(red: 103/255, green: 74/255, blue: 155/255, alpha: 100/100)
+    }
+
+}
