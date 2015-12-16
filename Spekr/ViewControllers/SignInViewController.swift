c538b1ae505b4c7c6d8e869f97d8e654de619bb3
Arjun <itsarjunn@gmail.com>
2015-12-16 17:03:44 +0530

    Facebook details(email, name & Display image) are now being stored in Parse Database

--- Spekr/ViewControllers/SignInViewController.swift
+++ Spekr/ViewControllers/SignInViewController.swift
@@ -23,6 +23,8 @@ class SignInViewController: UIViewController {
         navigationController?.navigationBarHidden = true
     }
     
+    
+    
     private func navigateToNewViewController(Identifier: String) {
         performSegueWithIdentifier(Identifier, sender: self)
     }
@@ -75,25 +77,114 @@ class SignInViewController: UIViewController {
         
     }
     
+    func returnFBUserDataToParse() {
+        
+        let requestParameters = ["fields": "id, email, name"]
+        
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: requestParameters)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+            }
+            else
+            {
+                let userId:String = result["id"] as! String
+                let userName:String? = result["name"] as? String
+                let userEmail:String? = result["email"] as? String
+                
+                
+                print("\(userEmail)")
+                
+                let myUser:PFUser = PFUser.currentUser()!
+                
+                // Save first name
+                if(userName != nil)
+                {
+                    myUser.setObject(userName!, forKey: "displayName")
+                    
+                }
+                
+                // Save email address
+                if(userEmail != nil)
+                {
+                    myUser.setObject(userEmail!, forKey: "email")
+                }
+                
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                    
+                    // Get Facebook profile picture
+                    let userProfile = "https://graph.facebook.com/" + userId + "/picture?type=large"
+                    
+                    let profilePictureUrl = NSURL(string: userProfile)
+                    
+                    let profilePictureData = NSData(contentsOfURL: profilePictureUrl!)
+                    
+                    if(profilePictureData != nil)
+                    {
+                        let profileFileObject = PFFile(data:profilePictureData!)
+                        myUser.setObject(profileFileObject!, forKey: "displayImage")
+                    }
+                    
+                    
+                    myUser.saveInBackgroundWithBlock({ (success:Bool, error:NSError?) -> Void in
+                        
+                        if(success)
+                        {
+                            print("User details are now updated")
+                        }
+                        
+                    })
+                    
+                    
+                    
+                }
+
+
+                
+            }
+        })
+    }
+    
     //Sign in with Facebook
     @IBAction func signInFacebook(sender: AnyObject) {
         
         let permissions = ["public_profile", "email"]
         PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) {
             (user: PFUser?, error: NSError?) -> Void in
-            if let user = user {
-                if user.isNew {
-                    print("User signed up and logged in through Facebook!")
+            
+            if error != nil {
+                //Display an alert message
+                let myAlert = UIAlertController(title:"Alert", message:error?.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert);
+                
+                let okAction =  UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+                
+                myAlert.addAction(okAction);
+                self.presentViewController(myAlert, animated:true, completion:nil);
+                
+                return
+            } else {
+                
+                if let user = user {
+                    if user.isNew {
+                        print("User signed up and logged in through Facebook!")
+                        
+                        self.returnFBUserDataToParse()
+                        
+                    } else {
+                        print("User logged in through Facebook!")
+                    }
+                    
+                    //Performing a segue to Local Feed Screen
+                    self.navigateToNewViewController("JumpFromSignInToLocalFeed")
+                    
                 } else {
-                    print("User logged in through Facebook!")
+                    print("Uh oh. The user cancelled the Facebook login.")
                 }
                 
-                //Performing a segue to Local Feed Screen
-                self.navigateToNewViewController("JumpFromSignInToLocalFeed")
-                
-            } else {
-                print("Uh oh. The user cancelled the Facebook login.")
             }
+            
         }
 
     }
