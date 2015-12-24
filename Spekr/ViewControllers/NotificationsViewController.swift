bb2f25b9c521b618b026eee75483b88e4606d1ef
Arjun <itsarjunn@gmail.com>
2015-12-24 23:14:37 +0530

    View controllers have been added. Small background color changes have been made.

---
+++ Spekr/ViewControllers/NotificationsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationsViewController.swift
+//  Spekr
+//
+//  Created by Arjun Kodur on 12/24/15.
+//  Copyright © 2015 Arjun Kodur. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        self.tabBarController?.navigationItem.title = "Notifications"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
